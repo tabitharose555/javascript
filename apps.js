093344d45751178c11e978576035df9db5910433
Iroegbu Chinwendu <tabitharose555@gmailcom>
2022-06-24 16:44:43 -0700

    my first git

---
+++ apps.js
@@ -0,0 +1,14 @@
+var name
+
+var height
+
+var country
+
+
+name=prompt("enter your name :","")
+
+height=prompt("enter your height :","")
+
+country=prompt("enter your country :","")
+
+document.write(name+ " is " +height+ " cm " +country+ " citizen ")
